a5ed07d245a82bc8afa1c993ccd987445e50d70d
Mathias <mathrb@gmail.com>
2021-01-23 15:19:29 +0100

    log exception, do not download restricted urls

--- mycollect/processors/url_grabber_processor.py
+++ mycollect/processors/url_grabber_processor.py
@@ -1,13 +1,17 @@
 """
 Processor that will get the content of a webpage
 """
-
 from typing import Optional
 
 from newspaper.article import Article, ArticleDownloadState
 
+from mycollect.logger import create_logger
 from mycollect.processors import Processor
 from mycollect.structures import MyCollectItem
+
+INVALID_URL_NEWS = [
+    "https://twitter.com/i/web/status"
+]
 
 
 class UrlGrabberProcessor(Processor):  # pylint:disable=too-few-public-methods
@@ -15,20 +19,32 @@ class UrlGrabberProcessor(Processor):  # pylint:disable=too-few-public-methods
     """Grab the content defined in the url of the MyCollectItem
     """
 
+    def __init__(self):
+        super().__init__()
+        self._logger = create_logger()
+
     def update_item(self, item: MyCollectItem) -> Optional[MyCollectItem]:
         """
             Updates the current MyCollectItem, return None to drop this item
         """
-        article = Article(item.url)
-        try:
-            article.download()
-            article.parse()
-        except Exception as err:  # pylint:disable=broad-except
-            print(err)
-        if article.download_state == ArticleDownloadState.SUCCESS:
-            item.extra["article"] = {
-                "text": article.text,
-                "title": article.title,
-                "keywords": article.keywords
-            }
+        if not self._is_restricted:
+            article = Article(item.url)
+            try:
+                article.download()
+                article.parse()
+            except Exception as err:  # pylint:disable=broad-except
+                self._logger.exception(err)
+            if article.download_state == ArticleDownloadState.SUCCESS:
+                item.extra["article"] = {
+                    "text": article.text,
+                    "title": article.title,
+                    "keywords": article.keywords
+                }
         return item
+
+    @staticmethod
+    def _is_restricted(url):
+        for invalid in INVALID_URL_NEWS:
+            if invalid in url:
+                return True
+        return False
